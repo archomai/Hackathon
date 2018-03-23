73ff211bdda218941525c092a7beadba173fb873
Seungri Cho <pseudo.circuit@gmail.com>
2018-03-23 18:12:37 +0900

    config/setting.py secret 오타 수정

--- app/config/settings.py
+++ app/config/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
-SECRET_DIR = os.path.join(ROOT_DIR, '.secrets')
+SECRET_DIR = os.path.join(ROOT_DIR, '.secret')
 SECRET_BASE = os.path.join(SECRET_DIR, 'base.json')
 
 secrets_base = json.loads(open(SECRET_BASE, 'rt').read())
